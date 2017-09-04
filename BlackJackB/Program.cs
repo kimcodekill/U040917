4466c25296045de8fdffe0e5f1522574e877e297
Joakim Linna <joakimlinna1998@gmail.com>
2017-09-04 21:50:32 +0200

    added BlackJackB

---
+++ BlackJackB/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cards = new string[52];
+            Random r = new Random();
+            string card = string.Empty;
+            int sum = 0;
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                card = string.Empty;
+                switch (r.Next(1, 4))
+                {
+                    case 1:
+                        card += 'H';
+                        break;
+                    case 2:
+                        card += 'K';
+                        break;
+                    case 3:
+                        card += 'R';
+                        break;
+                    case 4:
+                        card += 'S';
+                        break;
+                }
+
+                card += r.Next(1, 13);
+
+                //Cards aren't checked for dupes
+                cards[i] = card;
+            }
+
+            foreach (string s in cards)
+                Console.WriteLine(s);
+
+            //Edit so if-statement only checks for y/n
+            foreach (string s in cards)
+            {
+                Console.WriteLine("Vill du ha ett kort?");
+                if (Console.ReadLine().ToLower() == "ja")
+                {
+                    Console.Write(s + ' ');
+                    sum += Convert.ToInt32(s.Substring(1));
+                    if (sum <= 21)
+                        Console.WriteLine("(Poängsumma: " + sum + ')');
+                    else
+                    {
+                        Console.WriteLine("Du förlorade! (Poängsumma: " + sum + ')');
+                        break;
+                    }
+                }
+                else
+                    break;
+            }
+
+            if (sum == 21)
+                Console.WriteLine("Grattis 21!");
+        }
+    }
+}
